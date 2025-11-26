f65c8d5d5121b6e869b090fb11aac203cfeff74a
Vincent Mariucci <mariucci1@gmail.com>
2025-11-26 09:31:51 -0500

    finishing tutorial 3

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <genesis.h>
+#include <resources.h>
 
 int main() {
 
-    VDP_drawText("Hello Genesis World!", 8, 12);
+    VDP_loadTileSet(bgtile.tileset,1,DMA); //load the tileset into position 1 in VRAM, use Direct Memory Access
+    VDP_setTileMapXY(BG_B,1,2,2); //draw on Plane B, draw the tile that's in position 1 in VRAM, x position in tiles, y position in tiles
     while(1) {
         SYS_doVBlankProcess();
     }
